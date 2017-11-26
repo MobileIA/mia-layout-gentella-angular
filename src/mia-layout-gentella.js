ffeb3e282dbf52e3a795381a3a5a1fe6c1c6ccf9
Matias Camiletti <matias.camiletti@gmail.com>
2017-11-26 17:36:55 -0300

    Agregado controlador de Login.

--- src/mia-layout-gentella.js
+++ src/mia-layout-gentella.js
@@ -1,6 +1,6 @@
 /**
  * Libreria para usar el template Gentella
- * @version v0.1.1
+ * @version v0.0.4
  * @link https://github.com/MobileIA/mia-layout-gentella-angular
  * @author Matias Camiletti <matiascamiletti@mobileia.com> 
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -9,8 +9,37 @@
     'use strict';
 
     angular
-            .module('miaLayoutGentella', [])
+            .module('miaLayoutGentella', ['mobileiaAuthentication', 'ngAnimate', 'ui.router'])
+            .controller('GentellaLoginController', GentellaLoginController)
             .directive('gentellaFooter', gentellaFooter);
+    
+    GentellaLoginController.$inject = ['$scope', '$rootScope', 'mobileiaAuth', '$state'];
+
+    function GentellaLoginController($scope, $rootScope, mobileiaAuth, $state) {
+        var ctrl = this;
+        // Seteamos clase del body
+        $rootScope.bodyClass = "login";
+        // Setear variables al scope
+        $scope.title = $state.current.vars.title;
+        /**
+         * Funcion que se encarga de realizar el login.
+         */
+        ctrl.login = function(){
+            mobileiaAuth.signInWithEmailAndPassword({ 
+                email: $scope.email, 
+                password: $scope.password,
+                callback: function(success, error){
+                    if(!success){
+                        // No se ha podido loguear
+                        alert(error.message);
+                        return false;
+                    }
+                    // Redirigir a la home
+                    $state.go($state.current.vars.successState);
+                }
+            });
+        };
+    }
     
     gentellaFooter.$inject = [];
     
